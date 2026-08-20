99662b6990b5cbf88a3134143c9b890d044ffcee
MyaMonroe <211659085+MyaMonroe@users.noreply.github.com>
2026-08-20 02:16:13 -0500

    Initialize AKUJI App Check with Play Integrity

--- app/src/main/java/com/deffrow/akuji/MainActivity.kt
+++ app/src/main/java/com/deffrow/akuji/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance(),
+        )
+
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             AkujiApp()
